e4f940f987472e6ab146b0e3e0bad4f8fa8c47a3
Yogesh Panjwani <panjwaniyogesh95@gmail.com>
2021-07-05 11:57:16 +0530

    player behaviour added

--- player.py
+++ player.py
@@ -1,7 +1,16 @@
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
-FINISH_LINE_Y = 280
+FINISH_LINE_Y = 2
+from turtle import Turtle
 
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.setheading(90)
+        self.penup()
+        self.goto(STARTING_POSITION)
+
+    def go_up(self):
+        self.forward(MOVE_DISTANCE)
